7ff13573bf26e3d3fdcfc545d10376a530b1d24c
Matthew Broatch <mnbroatch@gmail.com>
2020-01-05 20:40:40 -0800

    broken but switching computers -_-

---
+++ src/JavaScriptAnimation/JavaScriptAnimation.stories.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { boolean } from '@storybook/addon-knobs';
+
+import JavaScriptAnimation from './index.js';
+import getYInShiftingSineWave from '../../utilities/getYInShiftingSineWave.js';
+import percentToHexColor from '../../utilities/percentToHexColor.js';
+
+export const JavaScriptAnimationStory = () => null
+
+export default {
+  title: 'JavaScriptAnimation',
+  component: JavaScriptAnimationStory
+}
