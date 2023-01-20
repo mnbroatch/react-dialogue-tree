dbb5ff0ecf341f0049b800614b06a2014fb5617c
Matthew Broatch <mnbroatch@gmail.com>
2023-01-19 19:46:28 -0800

    upgrade storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,15 @@
 const path = require('path');
 module.exports = {
   "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    // "../src/**/*.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    // "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "@storybook/preset-scss"
   ],
   "framework": {
     "name": "@storybook/react-webpack5",
